4b6c5516f836d9808f3cf9d1a2e5eb7bd02ddf31
Ana <dasilvaanacristina199@gmail.com>
2025-11-18 22:10:31 -0300

    Aula4layout

--- Aula 04/script.js
+++ Aula 04/script.js
@@ -1,36 +1,28 @@
-add()
-
-function add(){
-    //carregando o elemento ul que tem o ID "itens"
+function add() {
+    //Carregando o elemento ul que tem o ID "itens"
     var lista = document.getElementById('itens');
-
-    //carregando o elemento input que tem o ID "campo"
+    //Carregando o elemento input que tem o id "campo"
     var campo = document.getElementById('campo');
 
-    // evita item vazio
-    if (campo.value.trim() === "") return;
-
-    //Criando um elemento li
+    // Criando um elemento li
     var li = document.createElement('li');
 
-    //Insere o valor digitado dentro do input campo     usando a propriedade "value"
-    //para nosso elemento li
+    // Insere o valor digitado dentro do input campo usando a propiedade "value"
+    // para nosso elemento li
     li.innerHTML = campo.value;
 
-    // Criar botão de excluir
     var btn = document.createElement('button');
-    btn.innerHTML = "Apagar";
-    btn.addEventListener('onclick', apagarElemento());
+    btn.innerHTML = "Excluir";
+    btn.addEventListener('click', function () {
+        lista.removeChild(this.parentNode)
+    });
 
     li.appendChild(btn);
 
     //Aplicamos o elemento li dentro do elemento ul
     lista.appendChild(li);
 
-    //Limpamos a propriedade value do nosso elemento ul
+    //Limpamos a propriedade value do nosso elemento campo 
     campo.value = "";
-}
-
-function apagarElemento(elemento){
-    document.removeChild(elemento);
+    
 }
